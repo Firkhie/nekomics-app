92a3476e24ac7b68b297f74b8f60424bdabdd190
Firdig Alfalakhi <Alfalakhie@gmail.com>
2023-08-04 10:37:33 +0700

    feat: add port 80

--- server-side/app.js
+++ server-side/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
-const port = 3000 || process.env.PORT
+const port =  process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json()) // for parsing application/json
