bde27e64a6305506572ee5a0a0e089aec5e623c8
Firdig Alfalakhi <Alfalakhie@gmail.com>
2023-08-04 10:08:15 +0700

    feat: config express

--- server-side/app.js
+++ server-side/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 
 app.use(cors())
 app.use(express.json()) // for parsing application/json
